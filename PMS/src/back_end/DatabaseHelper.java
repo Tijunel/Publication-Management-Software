16d839515fbc662c5d77c286b19e1789b11e64f7
QifengLi <qi.li1@ucalgary.ca>
2018-11-14 12:43:15 -0700

    added the jdbc jar file
    the jdbc is now setup I will follow with xampp setup instructions

---
+++ PMS/src/back_end/DatabaseHelper.java
@@ -0,0 +1,37 @@
+package back_end;
+import java.sql.*;
+
+public class DatabaseHelper {
+	
+	String url = "jdbc:mysql://localhost:3306";
+	String user = "root";
+	String password = "";
+	
+	public void setupDatabase() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection con = DriverManager.getConnection(url, user, password);
+			Statement stmt = con.createStatement();
+			stmt.execute("CREATE DATABASE IF NOT EXISTS ensf480projectone");
+			stmt.execute("USE ensf480projectone");
+			stmt.execute("CREATE TABLE IF NOT EXISTS user ("
+					+ "id BIGINT NOT NULL AUTO_INCREMENT, "
+					+ "username VARCHAR(10) NOT NULL, "
+					+ "password VARCHAR(10) NOT NULL, "
+					+ "type VARCHAR(10) NOT NULL, "
+					+ "email VARCHAR(50), "
+					+ "PRIMARY KEY(id) "
+					+ ")");
+			stmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DatabaseHelper dbh = new DatabaseHelper();
+		dbh.setupDatabase();
+		System.out.println("done");
+	}
+}
